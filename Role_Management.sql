73783e8336280e1695d115d43944d213403923f6
Bryan Ng <79688274+bryanng77@users.noreply.github.com>
2025-01-21 16:12:56 +0800

    new upload

--- Role_Management.sql
+++ Role_Management.sql
@@ -1,14 +1,23 @@
--- Table Creation
-CREATE TABLE Roles (
-    RoleID INT PRIMARY KEY,           -- Unique identifier for roles
-    RoleName NVARCHAR(50) NOT NULL,   -- Descriptive role name
-    CanView BIT DEFAULT 1,            -- Whether the role can view data
-    CanEdit BIT DEFAULT 0             -- Whether the role can edit data
-);
 
--- Interseting data Roles Table
-INSERT INTO Roles (RoleID, RoleName, CanView, CanEdit)
-VALUES
-    (1, 'Admin', 1, 1),
-    (2, 'Manager', 1, 0),
-    (3, 'Employee', 1, 0);
+SELECT TOP 10
+    *
+FROM Roles;
+
+SELECT TOP 10
+    *
+FROM Users;
+
+SELECT
+    id
+    , r.RoleID
+    , RoleName
+    , CanView
+    , CanEdit
+    , FirstName
+    , LastName
+    , Email
+    , IsActive
+    ,DepartmentName
+FROM Roles r
+    INNER JOIN Users u
+    ON r.RoleID = u.RoleID;
